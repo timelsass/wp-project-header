b8a97383abb55b1e87bd18d4dcbde3acc8cd42cf
Tim <dev@tim.ph>
2019-02-25 01:58:49 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # wp-project-header
 Easily get a project's header file, theme or plugin.
+
+Don't go through the hassle of determining if your module is running in a theme or a plugin everytime you need to determine the projects header file.
+
+## Install
+
+Install with yarn:
+
+```
+$ yarn add wp-project-header --dev
+```
+
+Install with npm:
+
+```
+$ npm install wp-project-header --save-dev
+```
+
+## Usage
+
+```js
+const projectHeader = require( 'wp-project-header' );
+
+( async () => {
+	/* Inside a Plugin */
+	console.log( await projectHeader() );
+	//=> '/path/to/your-plugin/your-plugin.php';'
+
+	/* Inside a Theme */
+	console.log( await projectHeader() );
+	//=> '/path/to/your-theme/style.css';'
+} )();
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require( 'fs' ),
+	path = require( 'path' ),
+	fsPromises = fs.promises,
+	pkgDir = require( 'pkg-dir' );
+
+/**
+ * Automatically gets path/to/header for plugin or theme.
+ *
+ * If file is left empty then it will check to try and determine the right
+ * file for a plugin or theme. Search happens like this:
+ *
+ * 1. Use path/to/file.ext provided externally.
+ * 2. Check if project root contains plugin file, ie plugin-name/plugin-name.php
+ * 3. Check if project root contains style.css file, ie theme-name/style.css
+ *
+ * @param {String} file (Optional) Path to file.
+ */
+module.exports = async ( file ) => {
+	const dir = await pkgDir( __dirname );
+	file = file || dir + path.sep + path.basename( dir ) + '.php';
+	return await fsPromises.access( file, fs.constants.W_OK | fs.constants.R_OK ).catch( () => file = `${ dir + path.sep }style.css` );
+}
